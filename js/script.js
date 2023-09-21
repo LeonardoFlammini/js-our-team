d0a96d95d4456f3f0f87623de62f71a062937f00
LeonardoFlammini <leonardo98flammini@gmail.com>
2023-09-21 18:13:29 +0200

    FINISH

--- js/script.js
+++ js/script.js
@@ -45,16 +45,47 @@ const membersContainer = document.getElementById("members-container");
 
 // ********************** LET *************************//
 
-let card = ""; //potrebbe essere la const in un ciclo
+let cards = ""; //potrebbe essere la const in un ciclo
 
 // ********************** MAIN *************************//
 
-let memberString = "";
-for(member of teamMembers){
-  
-  for(label in member){
-    memberString += ` ${label} ${teamMembers[label]}`;
-  }
+reset();
 
-  console.log(memberString);
+printCards();
+
+// ********************** FUNCTIONS *************************//
+
+function reset ()
+{
+  membersContainer.innerHTML = "";
+}
+
+function printCards() 
+{
+  for(member of teamMembers){
+  
+    
+    cards = 
+    `
+
+    <div class="col my-3">
+      <div class="card-lf">
+        <div class="image-lf">
+          <img src="img/${member.image}" alt="">
+        </div>
+        <div class="info-lf text-center p-3 ">
+          <h5>${member.name}</h5>
+          <span>${member.role}</span>
+        </div>
+      </div>
+    </div>
+
+    `;
+  
+    membersContainer.innerHTML += cards;
+
+  
+  
+    console.log(cards);
+  }
 }
